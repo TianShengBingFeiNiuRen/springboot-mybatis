8d73c1940cf32470dfcff231fad1462b20c146c4
Andon <17610835229@163.com>
2021-11-17 17:06:08 +0800

    SpringBoot 整合 Mybatis

--- src/main/java/com/andon/springbootmybatis/SpringbootMybatisApplication.java
+++ src/main/java/com/andon/springbootmybatis/SpringbootMybatisApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.andon.springbootmybatis.mapper")
+@MapperScan({"com.andon.springbootmybatis.mapper"}) //扫描mapper接口文件所在包
 @SpringBootApplication
 public class SpringbootMybatisApplication {
 
